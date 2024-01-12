8937b388af2cfbf78b8e0f52aba494db84a9521d
wyattgust <wyattgust@outlook.com>
2024-01-11 19:17:07 -0500

    prep for week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage? ");
+        string stringPercent = Console.ReadLine();
+        int percent = int.Parse(stringPercent);
+        
+        string letter = "";
+        if (percent >= 90)
+            {
+                letter = "A";
+            }
+        else if (percent>= 80)
+            {
+                letter= "B";
+            }
+        
+        else if (percent>= 70)
+            {
+                letter= "C";
+            }
+        
+        else if (percent>= 60)
+            {
+                letter= "D";
+            }
+        else
+            {
+                letter=  "F";
+            }
+        Console.WriteLine($"Your grade is: {letter}");
+        
+
     }
 }
